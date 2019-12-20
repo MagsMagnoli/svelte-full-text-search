898b594766434abe7cf9e1264b570f0a57939ee1
Mags <dev@mags.page>
2019-12-20 10:06:45 -0800

    add example items

---
+++ src/data/example.js
@@ -0,0 +1,20 @@
+export const items = [
+  {
+    title: 'Title 1',
+    description:
+      'Description 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus ipsum vel purus luctus auctor.',
+    tags: ['tag1', 'tag1-1', 'tag1-2'],
+  },
+  {
+    title: 'Title 2',
+    description:
+      'Description 2. Nunc sodales orci vitae nisi hendrerit fermentum. Sed ac eleifend urna.',
+    tags: ['tag2', 'tag2-1', 'tag2-2'],
+  },
+  {
+    title: 'Title 3',
+    description:
+      'Description 3. Quisque bibendum risus et vehicula efficitur. Nulla lacinia, elit id pretium auctor, velit orci interdum elit, id pellentesque nisi mi sit amet lorem.',
+    tags: ['tag3', 'tag3-1', 'tag3-2'],
+  },
+];
